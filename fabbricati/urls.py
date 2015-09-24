138b7110a67bd30dbca1d48c7e8c79b9a0fe757f
Sergio <sergio@sergio.se>
2015-09-24 16:56:23 +0200

    edit index, add fabbricato.html

--- fabbricati/urls.py
+++ fabbricati/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 from django.conf import settings
-from .views import (index,)
+from .views import (index, fabbricato)
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 __author__ = 'sergio'
 
 urlpatterns = patterns('fabbricati.views',
                        url(r'^$', 'index', name='index'),
-
+                       url(r'^fabbricato/(?P<id_fab>\w+)$', 'fabbricato', name='fabbricato'),
                        )
+
+#debug
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += \
+        patterns('',
+                 (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                     'document_root': settings.MEDIA_ROOT}))
--- fabbricati/views.py
+++ fabbricati/views.py
@@ -11,11 +11,15 @@ def index(request):
         fabbricati = Fabbricato.objects.all()
 
 
-
         ctx = RequestContext(request,{'fabbricati':fabbricati})
         return render_to_response('pages/index.html',context_instance=ctx)
 
 
+def fabbricato(request,id_fab):
 
+   #get fabbricato
+    #id_fabbricato = request.GET['id']
+    fabbricato = Fabbricato.objects.get(id=id_fab)
 
-
+    ctx = RequestContext(request,{'fabbricato':fabbricato})
+    return render_to_response('pages/fabbricato.html',context_instance=ctx)
